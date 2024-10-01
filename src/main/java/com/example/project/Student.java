45538ff2f8accdb54d6ef44e4189c63ad67624d1
YesitongW <wuy2@nycstudents.net>
2024-10-01 13:02:31 +0000

    w2

--- src/main/java/com/example/project/Student.java
+++ src/main/java/com/example/project/Student.java
@@ -9,42 +9,52 @@ public class Student {
     // test score count (int) ..should initialize at 0
     // highest test score (double).. should initialize at 0.0
  
+    private String firstName;
+    private String lastName;
+    private int gradYear;
+    private double sumOfScores = 0.0;
+    private int testScoreCount = 0;
+    private double highestScore = 0.0;
+    private double averageTestScore = 0.0;
     // constructor
     public Student(String firstName, String lastName, int gradYear) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gradYear = gradYear;
         //implement code here!
-
     }
  
     // returns firstName
     public String getFirstName() {
         //implement code here!
-        return "";
+        return firstName;
     }
  
     // returns lastName
     public String getLastName() {
         //implement code here!
-        return "";
+        return lastName;
     }
  
     public double getHighestTestScore() {
         //implement code here!
-        return 0.0;
+        return highestScore;
     }
 
     public int getTestScoreCount(){
         //implement code here!
-        return 0;
+        return testScoreCount;
     }
 
     public int getGradYear(){
         //implement code here!
-        return 0;
+        return gradYear;
     }
  
     // sets gradYear to newGradYear
     public void setGradYear(int newGradYear) {
         //implement code here!
+        gradYear = newGradYear;
     }
  
     // adds newTestScore to accumulatedTestScores
@@ -52,31 +62,40 @@ public class Student {
     // set new highest test score 
     public void addTestScore(double newTestScore) {
         //implement code here!
+        sumOfScores += newTestScore;
+        testScoreCount ++;
+        if (newTestScore > highestScore){
+            highestScore = newTestScore;
+        }
     }
  
     // returns true if the student's average test score is greater
     // than or equal to 65; returns false otherwise (see Note 2 below)
     public boolean isPassing() {
         //implement code here!
-        return false;
+        if (sumOfScores / testScoreCount >= 65) {
+            return true;
+        } else {
+            return false;
+        }
     }
  
     // returns the Student's average test score as the
     // quotient of accumulatedTestScores and testScoreCount
     public double averageTestScore() {
         //implement code here!
-        return 0.0;
+        return sumOfScores / testScoreCount;
     }
  
     // this method prints all info of a Student object to the console 
     // I AM NOT TESTING YOU ON THIS METHOD. IT'S FOR YOUR TESTING PURPOSES ONLY.. you don't have to use it
     public void printStudentInfo() {
-        System.out.println("Student Full Name: " );
-        System.out.println("Graduation Year: ");
-        System.out.println("Number of tests: ");
-        System.out.println("Average Test Score: ");
-        System.out.println("Highest Test Score: ");
-        System.out.println("Is passing: ");
+        System.out.println("Student Full Name: " + firstName + lastName );
+        System.out.println("Graduation Year: "+ gradYear);
+        System.out.println("Number of tests: "+ testScoreCount);
+        System.out.println("Average Test Score: "+ averageTestScore);
+        System.out.println("Highest Test Score: " + highestScore);
+        System.out.println("Is passing: " + (isPassing()? "yes" : "no"));
     }
  }
  
--- src/main/java/com/example/project/StudentRunner.java
+++ src/main/java/com/example/project/StudentRunner.java
@@ -1,5 +1,10 @@
+package com.example.project;
+
 public class StudentRunner{
     public static void main(String[] args) {
         //use this StudentRunner class if you need to test your code 
+        Student stu1 = new Student("John","Cat",2026);
+        stu1.addTestScore(97.5);
+        
     }
 }
